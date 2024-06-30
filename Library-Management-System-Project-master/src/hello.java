fdc05fc0e0fc136c19c0604a708eede66270a5e4
Preetisharma2004 <99601660+Preetisharma2004@users.noreply.github.com>
2024-06-30 22:59:56 +0530

    Add files via upload

---
+++ Library-Management-System-Project-master/src/hello.java
@@ -0,0 +1,14 @@
+/*
+ * Here comes the text of your license
+ * Each line should be prefixed with  * 
+ */
+
+/**
+ *
+ * @author Preeti sharma
+ */
+public class hello {
+  public static void main(String[] args) {
+      System.out.println("Hello, Java Chat Application!");
+  }
+}
